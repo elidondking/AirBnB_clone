808dfa4c22e29a46207c251b67b64c07672cc806
Emaye Andrew <77851267+andycool88@users.noreply.github.com>
2022-11-27 22:05:16 +0100

    Add files via upload

---
+++ models/engine/file_storage.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Contains the file storage class model"""
+
+import json
+
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+
+
+class FileStorage:
+    """
+       serializes instances to a JSON file and
+       deserializes JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Returns the dictionary __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        sets in __objects the `obj` with key <obj class name>.id
+        """
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+
+    def save(self):
+        """
+        Serialize __objects to the JSON file
+        """
+        with open(self.__file_path, mode="w") as f:
+            dict_storage = {}
+            for key, value in self.__objects.items():
+                dict_storage[key] = value.to_dict()
+            json.dump(dict_storage, f)
+
+    def reload(self):
+        """
+        Deserializes the JSON file to __objects
+        -> Only IF it exists!
+        """
+        classes = {
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
+        try:
+            with open(self.__file_path, 'r') as f:
+                jo = json.load(f)
+            for key in jo:
+                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+        except FileNotFoundError:
+            pass
